54ca0eb5ce7478924107db8e65c4427cc193114f
Toàn <contact.pmtoan@gmail.com>
2022-12-30 03:36:47 +0700

    Server
    Update route for interbank and otp; update use cor with any origin

--- server/endpoint.js
+++ server/endpoint.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import BeneficiaryRouter from './components/Beneficiary/Beneficiary.route.js';
 import TransactionRouter from './components/Transaction/Transaction.route.js';
+import InterbankRouter from './components/InterbankAPI/Interbank.route.js';
+import OTPRouter from './components/OTP/OTP.route.js';
 
 import UserRoute from './components/User/User.route.js';
 
@@ -9,5 +11,7 @@ const router = new Router();
 router.use('/user', [UserRoute]);
 router.use('/api/beneficiary', BeneficiaryRouter);
 router.use('/api/transaction', TransactionRouter);
+router.use('/api/interbank', InterbankRouter);
+router.use('/api/otp', OTPRouter);
 
 export default router;
