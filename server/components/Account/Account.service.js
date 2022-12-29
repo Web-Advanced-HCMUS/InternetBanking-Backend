2d1170e2403e2d1841b42882ebe562e3d60dac2c
Toàn <contact.pmtoan@gmail.com>
2022-12-30 03:27:53 +0700

    Account
    Craete Account component v.1.0

---
+++ server/components/Account/Account.service.js
@@ -0,0 +1,47 @@
+import { errorMessage } from '../../utils/error.js';
+import AccountModel from '../model/Account.model.js';
+import APIError from '../../utils/APIError.js';
+
+export async function getAccount(accountNumber) {
+  try {
+    return await AccountModel.findOne({ accountNumber });
+  } catch (error) {
+    throw new APIError(500, error.message);
+  }
+}
+
+export async function checkBalanceAfterSpend(accountNumber, amount, fee) {
+  try {
+    const account = await AccountModel.findOne({ accountNumber });
+
+    return (account.currentBalance - amount - fee) > 0;
+  } catch (error) {
+    throw new APIError(500, error.message);
+  }
+}
+
+export async function addMoneyToAccount(accountNumber, amount) {
+  try {
+    const account = await AccountModel.findOne({ accountNumber });
+
+    return await AccountModel.updateOne(
+      { accountNumber },
+      { $set: { currentBalance: account.currentBalance + amount } }
+    );
+  } catch (error) {
+    throw new APIError(500, error.message);
+  }
+}
+
+export async function subtractMoneyFromAccount(accountNumber, amount) {
+  try {
+    const account = await AccountModel.findOne({ accountNumber });
+
+    return await AccountModel.updateOne(
+      { accountNumber },
+      { $set: { currentBalance: account.currentBalance - amount } }
+    );
+  } catch (error) {
+    throw new APIError(500, error.message);
+  }
+}
