ab7a9002b7a97031ddee7ca4b3fc02f96b3560ac
Toàn <contact.pmtoan@gmail.com>
2023-01-05 16:39:28 +0700

    MODEL
    Change in Debt and Transaction model

--- server/components/model/Debt.model.js
+++ server/components/model/Debt.model.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-import { DEBT_STATUS, DEBT_TYPE } from '../../utils/constant.js';
+import { DEBT_STATUS } from '../../utils/constant.js';
 
 const DebtSchema = new mongoose.Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: 'UserInfo', require: true },
-  otherUserId: { type: mongoose.Types.ObjectId, ref: 'UserInfo', require: true },
+  creditorAccountNumber: { type: String, require: true },
+  debtorAccountNumber: { type: String, require: true },
   amountOwed: { type: Number, require: true },
-  debtType: { type: String, enum: Object.values(DEBT_TYPE), require: true },
+  content: { type: String, require: true },
   status: { type: String, enum: Object.values(DEBT_STATUS), default: DEBT_STATUS.INCOMPLETE, require: true },
   startDate: { type: Date, default: new Date(), require: true },
   endDate: { type: Date, require: true }
