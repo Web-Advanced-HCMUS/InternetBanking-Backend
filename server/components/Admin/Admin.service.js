0b3308d21e44217265aee02d137ec7c433c8c7f2
Huynh.Duc.Le <huynhducle20000@gmail.com>
2023-01-07 23:54:57 +0700

    Fix code User

--- server/components/Admin/Admin.service.js
+++ server/components/Admin/Admin.service.js
@@ -6,4 +6,49 @@ import { errorMessage } from '../../utils/error.js';
 import EmployeeModel from '../model/Employee.model.js';
 import UserLoginModel from '../model/UserLogin.model.js';
 
-import { generateAccessToken } from '../User/User.service.js';
+export async function updateEmployeeService(empId, body) {
+  try {
+    const findEmp = await EmployeeModel.findOne({ empId }).lean();
+    if (!findEmp) return errorMessage(404, 'NOT FOUND');
+
+    await EmployeeModel.findOneAndUpdate({ empId }, body);
+
+    return true;
+  } catch (error) {
+    return errorMessage(500, error);
+  }
+}
+
+export async function deleteEmployeeService(empId) {
+  try {
+    const findEmp = await EmployeeModel.findOne({ empId }).lean();
+    if (!findEmp) return errorMessage(404, 'NOT FOUND');
+
+    const findLogin = await UserLoginModel.findOne({ userId: findEmp?._id }).lean();
+    if (!findLogin) return errorMessage(404, 'NOT FOUND');
+
+    await Promise.all([
+      EmployeeModel.findOneAndDelete({ empId }),
+      UserLoginModel.findOneAndDelete({ userId: findEmp?._id })
+    ]);
+
+    return true;
+  } catch (error) {
+    return errorMessage(500, error);
+  }
+}
+
+export async function getListEmployeeService(skip, limit) {
+  try {
+    const [payload, count] = await Promise.all([
+      UserLoginModel.countDocuments({}),
+      UserLoginModel.find({}).populate('userId')
+      .skip(skip).limit(limit)
+      .lean()
+    ]);
+
+    return [count, payload];
+  } catch (error) {
+    return errorMessage(error);
+  }
+}
--- server/components/User/User.service.js
+++ server/components/User/User.service.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 import { errorMessage } from '../../utils/error.js';
 
@@ -11,7 +12,9 @@ import EmployeeModel from '../model/Employee.model.js';
 
 import { nodeMailerSendEmail } from '../../helper/mailer.js';
 
-import { USER_ROLE, USER_GENDER, USER_MODEL_TYPE } from '../../utils/constant.js';
+import {
+  USER_ROLE, USER_GENDER, USER_MODEL_TYPE, HASH_DIGIT
+} from '../../utils/constant.js';
 
 import { recoverPasswordMail } from '../../utils/mailTemplate/recoveryPassword.mailTemplate.js';
 import { createAccountOTPMail } from '../../utils/mailTemplate/createAccount.mailTemplate.js';
@@ -66,8 +69,11 @@ export async function generateOTPService(userId) {
 export async function createUserService(auth, body) {
   try {
     const {
-      username, password, phone, identityCard, email, role, gender, accountType
+      username, phone, identityCard, email, role, gender, accountType
     } = body;
+
+    let { password } = body;
+    password = bcrypt.hashSync(password, HASH_DIGIT);
 
     const { _id } = auth;
 
@@ -198,7 +204,7 @@ export async function userLoginService(body) {
 
     if (!user) return errorMessage(404, 'USER NOT FOUND!');
 
-    if (!(user?.password === password)) return errorMessage(405, 'WRONG PASSWORD!');
+    if (!bcrypt.compareSync(user?.password, password)) return errorMessage(405, 'WRONG PASSWORD!');
 
     if (!user?.userId?.isActivated) return errorMessage(405, 'UNACTIVED!');
 
@@ -241,10 +247,10 @@ export async function getListUserService(skip, limit) {
 
 export async function sendMailForgotPasswordService(username) {
   try {
-    const hasUser = await UserLoginModel.findOne({ username }).lean();
+    const hasUser = await UserLoginModel.findOne({ username }).populate('userId').lean();
     if (!hasUser) return errorMessage(404, 'NOT FOUND!');
 
-    const { fullName, email, _id } = hasUser;
+    const { fullName, email, _id } = hasUser.userId;
 
     // Generate OTP
     const otp = await generateOTPService(_id, 'Forgot Password');
@@ -275,8 +281,9 @@ export async function forgotPasswordService(username, otp, newPass) {
     const currentTime = moment();
     if (hasOTP?.expiredTime < currentTime) { return errorMessage(405, 'OTP Expired!'); }
 
+    const hashPass = bcrypt.hashSync(newPass, HASH_DIGIT);
     const { _id } = hasOTP.userId;
-    await UserInfoModel.findByIdAndUpdate(_id, { password: newPass });
+    await UserLoginModel.findByIdAndUpdate(_id, { password: hashPass });
 
     await UserOTPModel.findByIdAndDelete(hasOTP._id);
 
@@ -292,9 +299,10 @@ export async function changePasswordService(auth, oldPass, newPass) {
     const hasUser = await UserLoginModel.findOne({ userId: _id }).lean();
     if (!hasUser) return errorMessage(404, 'NOT FOUND!');
 
-    if (hasUser?.password !== oldPass) return errorMessage(405, 'WRONG OLD PASSWORD!');
+    if (!bcrypt.compareSync(hasUser?.password, oldPass)) return errorMessage(405, 'WRONG OLD PASSWORD!');
 
-    await UserLoginModel.findOneAndUpdate({ userId: _id }, { password: newPass });
+    const hashPass = bcrypt.hashSync(newPass);
+    await UserLoginModel.findOneAndUpdate({ userId: _id }, { password: hashPass });
 
     return true;
   } catch (error) {
