8f5403ada4795232568222391df9a80583c17136
Toàn <contact.pmtoan@gmail.com>
2023-01-08 16:17:34 +0700

    Update interbank document

--- server/components/InterbankAPI/Interbank.docs.js
+++ server/components/InterbankAPI/Interbank.docs.js
@@ -8,76 +8,78 @@
 /**
  * @swagger
  * definitions:
+ *  InterbankTransaction:
+ *      type: object
+ *      properties:
+ *          fromAccountNumber:
+ *              type: string
+ *              description: The account number of sender
+ *          fromAccountOwnerName:
+ *              type: string
+ *              description: The account name of sender
+ *          bank:
+ *              type: string
+ *              description: Bank code of linked bank
+ *          toAccountNumber:
+ *              type: string
+ *              description: The account number of receiver
+ *          toAccountOwnerName:
+ *              type: string
+ *              description: The account name of receiver
+ *          transactionType:
+ *              type: string
+ *              description: The type of transaction
+ *          feePaymentMethod:
+ *              type: string
+ *              description: Type of fee payment
+ *              enum: ["paid sender", "paid receiver"]
+ *          amount:
+ *              type: number
+ *              description: Amount of transaction
+ *          fee:
+ *              type: number
+ *              description: Fee of transaction
+ *          content:
+ *              type: string
+ *              description: Content of transaction
+ *          status:
+ *              type: string
+ *              description: Status of transaction
+ *          time:
+ *              type: string
+ *              description: The time which transaction is operated
+ *          signature:
+ *              type: string
+ *              description: Your bank's signature use to verify transaction
+ *      example:
+ *          fromAccountNumber: "9021762999999"
+ *          fromAccountOwnerName: Messi
+ *          bank: FB88NCCA
+ *          toAccountNumber: "9021762797979"
+ *          toAccountOwnerName: Ronaldo
+ *          transactionType: interbank-transfer
+ *          feePaymentMethod: paid receiver
+ *          amount: 10000
+ *          fee: 999
+ *          content: chuyển tiền
+ *          status: success
+ *          signature: "dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA=="
+ *          time: 2023-01-05T10:59:47.359Z
+ *          _id: 63b6ada68c53dae7211a339b
+ *
  *  ResponseInterbankTransaction:
  *      type: object
  *      properties:
  *          data:
- *              type: object
- *              properties:
- *                  fromAccountNumber:
- *                      type: string
- *                      description: The account number of sender
- *                  fromAccountOwnerName:
- *                      type: string
- *                      description: The account name of sender
- *                  bank:
- *                      type: string
- *                      description: Bank code of linked bank
- *                  toAccountNumber:
- *                      type: string
- *                      description: The account number of receiver
- *                  toAccountOwnerName:
- *                      type: string
- *                      description: The account name of receiver
- *                  transactionType:
- *                      type: string
- *                      description: The type of transaction
- *                  feePaymentMethod:
- *                      type: string
- *                      description: Type of fee payment
- *                      enum: ["paid sender", "paid receiver"]
- *                  amount:
- *                      type: number
- *                      description: Amount of transaction
- *                  fee:
- *                      type: number
- *                      description: Fee of transaction
- *                  content:
- *                      type: string
- *                      description: Content of transaction
- *                  status:
- *                      type: string
- *                      description: Status of transaction
- *                  time:
- *                      type: string
- *                      description: The time which transaction is operated
- *                  signature:
- *                      type: string
- *                      description: Your bank's signature use to verify transaction
+ *              $ref: '#/definitions/InterbankTransaction'
  *          signature:
  *              type: string
  *              description: Our bank's signature use to verify transaction
+ *              example: "dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA=="
  *          publicKey:
  *              type: string
  *              description: Our bank's public key use to verify signature
- *      example:
- *          data:
- *              fromAccountNumber: "9021762999999"
- *              fromAccountOwnerName: Messi
- *              bank: FB88NCCA
- *              toAccountNumber: "9021762797979"
- *              toAccountOwnerName: Ronaldo
- *              transactionType: interbank-transfer
- *              feePaymentMethod: paid receiver
- *              amount: 10000
- *              fee: 999
- *              content: chuyển tiền
- *              status: success
- *              signature: dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA==
- *              time: 2023-01-05T10:59:47.359Z
- *              _id: 63b6ada68c53dae7211a339b
- *          signature: dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA==
- *          publicKey: -----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvdXEiU3vWemBfpWqyaLE7f5YSvB/cbkXpv5LRo8mWXea4qVdCa0e\na2uUyZcNLgWJzOGzbLrGk1r4wBbPHuR+mIz2tHEYRCcUleHH60RhivDYeL8ZlC7B\n1emvYGJ5DtLK+icO7XwVKdwO77mm6VepPXih8Rh7YzYFSQkZXUkjJRJu81ARPUWF\nNR5x6Fy0oMJ3BN80vQJth+IZ8erfvr2IKsl66MmjBOV5Iesz6NprezFRSwoi2ZqW\nWd7n68nY8oS9H65v3RWID/rnl8a4psupdxAdHbQVwNzquVZN/WBSCvlZgqsrdTDe\n/SMouMVxJf6/399fGPoyWf3Kv5HE9cwVTQIDAQAB\n-----END RSA PUBLIC KEY-----\n
+ *              example: "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvdXEiU3vWemBfpWqyaLE7f5YSvB/cbkXpv5LRo8mWXea4qVdCa0e\na2uUyZcNLgWJzOGzbLrGk1r4wBbPHuR+mIz2tHEYRCcUleHH60RhivDYeL8ZlC7B\n1emvYGJ5DtLK+icO7XwVKdwO77mm6VepPXih8Rh7YzYFSQkZXUkjJRJu81ARPUWF\nNR5x6Fy0oMJ3BN80vQJth+IZ8erfvr2IKsl66MmjBOV5Iesz6NprezFRSwoi2ZqW\nWd7n68nY8oS9H65v3RWID/rnl8a4psupdxAdHbQVwNzquVZN/WBSCvlZgqsrdTDe\n/SMouMVxJf6/399fGPoyWf3Kv5HE9cwVTQIDAQAB\n-----END RSA PUBLIC KEY-----\n"
  *
  *  CreateInterbankTransaction:
  *      type: object
@@ -129,8 +131,8 @@
  *              amount: 10000
  *              fee: 999
  *              content: chuyển tiền
- *          signature: dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA==
- *          publicKey: -----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvdXEiU3vWemBfpWqyaLE7f5YSvB/cbkXpv5LRo8mWXea4qVdCa0e\na2uUyZcNLgWJzOGzbLrGk1r4wBbPHuR+mIz2tHEYRCcUleHH60RhivDYeL8ZlC7B\n1emvYGJ5DtLK+icO7XwVKdwO77mm6VepPXih8Rh7YzYFSQkZXUkjJRJu81ARPUWF\nNR5x6Fy0oMJ3BN80vQJth+IZ8erfvr2IKsl66MmjBOV5Iesz6NprezFRSwoi2ZqW\nWd7n68nY8oS9H65v3RWID/rnl8a4psupdxAdHbQVwNzquVZN/WBSCvlZgqsrdTDe\n/SMouMVxJf6/399fGPoyWf3Kv5HE9cwVTQIDAQAB\n-----END RSA PUBLIC KEY-----\n
+ *          signature: "dNUNrm11plLe3HlXp73siEzDMSHQuA4bHwZGR09ay4Ilo7o4rfczy6EIc3YhIpuwFJOniJaAJnfWwNChew5xIhFmQALMJd0UeivavHva02VPg8V2Rku3Ohrdm/qLamKA27U6bp5FcnId30MyRR45TurssWovPQUlBlDlBMVEiwbhzbzlTtCF9aufx22/p8peXP8ykTa4W9VPlk501hmF7Md2J/fgiXXXJxXlLqwgNFtJvFPd8pItBnKn6XqjU9EBl2Y2SGyIhyy+bLh5VYMaWZjU59z/p9OJFHPNBAEuuQQvdBM9QDqT6gTMmn28aPrEOUopEoJwKvyi7ucqIGzBwA=="
+ *          publicKey: "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvdXEiU3vWemBfpWqyaLE7f5YSvB/cbkXpv5LRo8mWXea4qVdCa0e\na2uUyZcNLgWJzOGzbLrGk1r4wBbPHuR+mIz2tHEYRCcUleHH60RhivDYeL8ZlC7B\n1emvYGJ5DtLK+icO7XwVKdwO77mm6VepPXih8Rh7YzYFSQkZXUkjJRJu81ARPUWF\nNR5x6Fy0oMJ3BN80vQJth+IZ8erfvr2IKsl66MmjBOV5Iesz6NprezFRSwoi2ZqW\nWd7n68nY8oS9H65v3RWID/rnl8a4psupdxAdHbQVwNzquVZN/WBSCvlZgqsrdTDe\n/SMouMVxJf6/399fGPoyWf3Kv5HE9cwVTQIDAQAB\n-----END RSA PUBLIC KEY-----\n"
  *
  */
 
