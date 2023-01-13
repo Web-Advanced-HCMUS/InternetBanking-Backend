8337418113cc971310eb3f25253cbed0979d685d
Toàn <contact.pmtoan@gmail.com>
2023-01-13 12:41:08 +0700

    Update model

--- server/components/model/Beneficiary.model.js
+++ server/components/model/Beneficiary.model.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import { TRANSFER_TYPE } from '../../utils/constant.js';
+import {BANK_CODE, TRANSFER_TYPE} from '../../utils/constant.js';
 
 const BeneficiarySchema = new mongoose.Schema({
   userId: { type: mongoose.Types.ObjectId, ref: 'UserInfo', require: true },
   reminiscentName: { type: String, require: true },
   accountNumber: { type: String, require: true },
   type: { type: String, enum: Object.values(TRANSFER_TYPE), require: true },
-  interBank: { type: mongoose.Types.ObjectId, ref: 'InterBank' }
+  bankCode: { type: String, default: BANK_CODE.MY_BANK }
 }, { collection: 'Beneficiaries', versionKey: false });
 
 export default mongoose.model('Beneficiary', BeneficiarySchema);
--- server/components/model/UserOTP.model.js
+++ server/components/model/UserOTP.model.js
@@ -3,9 +3,9 @@ import { OTP_STATUS } from '../../utils/constant.js';
 
 const UserOTPSchema = new mongoose.Schema({
   userId: { type: mongoose.Types.ObjectId, ref: 'UserInfo', require: true },
-  otp: { type: String },
-  status: { type: String, enum: Object.values(OTP_STATUS) },
-  expiredTime: { type: Date }
+  otp: { type: String, require: true },
+  status: { type: String, enum: Object.values(OTP_STATUS), require: true },
+  expiredTime: { type: Date, require: true }
 }, { collection: 'UserOTP', versionKey: false });
 
 export default mongoose.model('UserOTP', UserOTPSchema);
